fa289569637f46a077d56f68cd6c2cac1e396081
Andrea Gunderson <agunde@bitwise.io>
2018-03-20 16:21:20 -0500

    Remove unused create and unreachable code
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

--- tp/src/handler.rs
+++ tp/src/handler.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 use protobuf;
-use protobuf::Message;
-use protobuf::MessageStatic;
 use protobuf::RepeatedField;
 
 use crypto::digest::Digest;
@@ -748,7 +746,6 @@ impl TransactionHandler for SabreTransactionHandler {
                 signer,
                 &mut state,
             ),
-            _ => Err(ApplyError::InvalidTransaction("Invalid action".into())),
         }
     }
 }
