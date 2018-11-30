648b5e3477cb3fb589fc4091a649ee67b58b5951
Peter Schwarz <pschwarz@bitwise.io>
2018-11-30 14:08:09 -0600

    Add State-related traits
    Add the Read, Write, and Prune traits.  These traits provide read
    and commit operations, respectively, on an underlying storage mechanism.
    
    Reading simply amounts to fetching multiple keys from state, and
    returning the results in a map (a standard multi-get-style operation).
    
    The Write and Prune operations in detail:
    - commit
    - compute_next_state_id
    - prune
    
    `commit`, as it implies, will actually write the values to the underlying
    storage mechanism and return a new state id.
    
    `compute_next_state_id` is effectively a dry-run of commit, providing
    the same state id, but not actually writing the values to the underlying
    storage mechanism.
    
    `prune` will remove old keys that are no longer relevant prior to a
    given state id.  This operation only applies to storage mechanisms that
    can track change in state ids over time.
    
    Additionally,
    
    - Provides custom errors for the three types of state operations
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

---
+++ libtransact/src/state/error.rs
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2018 Bitwise IO, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -----------------------------------------------------------------------------
+ */
+
+use std::error::Error;
+use std::fmt;
+
+/// An error that may occur on state writes.
+#[derive(Debug)]
+pub enum StateWriteError {
+    /// Attempted to write to state from a given state id, and that state id
+    /// does not exist
+    InvalidStateId(String),
+    /// An error occurred with the underlying storage mechanism
+    StorageError(Box<dyn Error>),
+}
+
+impl fmt::Display for StateWriteError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            StateWriteError::InvalidStateId(msg) => write!(f, "Invalid State Id: {}", msg),
+            StateWriteError::StorageError(err) => write!(f, "Storage Error: {}", err.description()),
+        }
+    }
+}
+
+impl Error for StateWriteError {
+    fn description(&self) -> &str {
+        match self {
+            StateWriteError::InvalidStateId(_) => "An invalid State Id was provided.",
+            StateWriteError::StorageError(_) => {
+                "An error occurred with the underlying storage layer."
+            }
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match self {
+            StateWriteError::InvalidStateId(_) => None,
+            StateWriteError::StorageError(err) => Some(err.as_ref()),
+        }
+    }
+}
+
+/// An error that may occur on state reads.
+#[derive(Debug)]
+pub enum StateReadError {
+    /// Attempted to read to state from a given state id, and that state id
+    /// does not exist.
+    InvalidStateId(String),
+    /// An poorly formed or invalid key was provided.
+    InvalidKey(String),
+    /// An error occurred with the underlying storage mechanism
+    StorageError(Box<dyn Error>),
+}
+
+impl fmt::Display for StateReadError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            StateReadError::InvalidStateId(msg) => write!(f, "Invalid State Id: {}", msg),
+            StateReadError::InvalidKey(key) => write!(f, "Invalid Key: {}", key),
+            StateReadError::StorageError(err) => write!(f, "Storage Error: {}", err.description()),
+        }
+    }
+}
+
+impl Error for StateReadError {
+    fn description(&self) -> &str {
+        match self {
+            StateReadError::InvalidStateId(_) => "An invalid State Id was provided.",
+            StateReadError::InvalidKey(_) => "A provided key was invalid",
+            StateReadError::StorageError(_) => {
+                "An error occurred with the underlying storage layer."
+            }
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match self {
+            StateReadError::InvalidStateId(_) | StateReadError::InvalidKey(_) => None,
+            StateReadError::StorageError(err) => Some(err.as_ref()),
+        }
+    }
+}
+
+/// An error that may occur on a state prune.
+#[derive(Debug)]
+pub enum StatePruneError {
+    /// Attempted to prune a state id that does not exist.
+    InvalidStateId(String),
+    /// An error occurred with the underlying storage mechanism.
+    StorageError(Box<dyn Error>),
+}
+
+impl fmt::Display for StatePruneError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            StatePruneError::InvalidStateId(msg) => write!(f, "Invalid State Id: {}", msg),
+            StatePruneError::StorageError(err) => write!(f, "Storage Error: {}", err.description()),
+        }
+    }
+}
+
+impl Error for StatePruneError {
+    fn description(&self) -> &str {
+        match self {
+            StatePruneError::InvalidStateId(_) => "An invalid State Id was provided.",
+            StatePruneError::StorageError(_) => {
+                "An error occurred with the underlying storage layer."
+            }
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match self {
+            StatePruneError::InvalidStateId(_) => None,
+            StatePruneError::StorageError(err) => Some(err.as_ref()),
+        }
+    }
+}
---
+++ libtransact/src/state/mod.rs
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2018 Bitwise IO, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -----------------------------------------------------------------------------
+ */
+
+//! Methods for interacting with State.
+//!
+//! Transact State is managed via the implementation of three traits: the
+//! `Write`, `Read`, and `Prune`.  These provide commit, read access,
+//! and a way to purge old state, respectively, to an underlying storage mechanism.
+
+mod error;
+
+use crate::receipts::StateChange;
+pub use crate::state::error::{StatePruneError, StateReadError, StateWriteError};
+use std::collections::HashMap;
+
+/// `state::Write` provides a way to write to a particular state storage system.
+///
+/// It provides the ability for the caller to either compute the next `StateId` -
+/// useful for validating expected results - or committing the results to an
+/// underlying store.
+///
+/// A `StateId`, in the context of Write, is used to indicate the
+/// starting state on which the changes will be applied.  It can be thought of
+/// as the identifier of a checkpoint or snapshot.
+///
+/// All operations are made using `StateChange` instances. These are the
+/// ordered set of changes to be applied onto the given `StateId`.
+///
+/// Implementations are expected to be thread-safe.
+pub trait Write: Sync + Send + Clone {
+    /// A reference to a checkpoint in state. It could be a merkle hash for
+    /// a merkle database.
+    type StateId;
+    /// The Key that is being stored in state.
+    type Key;
+    /// The Value that is being stored in state.
+    type Value;
+
+    /// Given a `StateId` and a slice of `StateChange` values, persist the
+    /// state changes and return the resulting next `StateId` value.
+    ///
+    /// This function will persist the state values to the
+    /// underlying storage mechanism.
+    ///
+    /// # Errors
+    ///
+    /// Any issues with committing the processing results will return a
+    /// `StateWriteError`.
+    fn commit(
+        &self,
+        state_id: &Self::StateId,
+        state_changes: &[StateChange<Self::Key, Self::Value>],
+    ) -> Result<Self::StateId, StateWriteError>;
+
+    /// Given a `StateId` and a slice of `StateChange` values, compute the
+    /// next `StateId` value.
+    ///
+    /// This function will compute the value of the next `StateId` without
+    /// actually persisting the state changes. Effectively, it is a dry-run.
+    ///
+    /// Returns the next `StateId` value;
+    ///
+    /// # Errors
+    ///
+    /// `StateWriteError` is returned if any issues occur while trying to
+    /// generate this next id.
+    fn compute_state_id(
+        &self,
+        state_id: &Self::StateId,
+        state_changes: &[StateChange<Self::Key, Self::Value>],
+    ) -> Result<Self::StateId, StateWriteError>;
+}
+
+/// `state::Prune` provides a way to remove state ids from a particular state
+/// storage system.
+///
+/// Removing `StateIds` and the associated state makes it so the state storage
+/// system does not grow unbounded.
+pub trait Prune: Sync + Send + Clone {
+    /// A reference to a checkpoint in state. It could be a merkle hash for
+    /// a merkle database.
+    type StateId;
+    /// The Key that is being stored in state.
+    type Key;
+    /// The Value that is being stored in state.
+    type Value;
+
+    /// Prune any State prior to the given StateId.
+    ///
+    /// In storage mechanisms that have a concept of `StateId` ordering, this
+    /// function should provide the functionality to prune older state values.
+    ///
+    /// It can be considered a clean-up or space-saving mechanism.
+    ///
+    /// It returns the keys that have been removed from state, if any.
+    ///
+    /// # Errors
+    ///
+    /// StateWriteError is returned if any issues occur while trying to
+    /// prune past results.
+    fn prune(&self, state_ids: Vec<Self::StateId>) -> Result<Vec<Self::Key>, StateWriteError>;
+}
+
+/// `state::Read` provides a way to retrieve state from a particular storage
+/// system.
+///
+/// It provides the ability to read values from an underlying storage
+///
+/// Implementations are expected to be thread-safe.
+pub trait Read: Sync + Send + Clone {
+    /// A reference to a checkpoint in state. It could be a merkle hash for
+    /// a merkle database.
+    type StateId;
+    /// The Key that is being stored in state.
+    type Key;
+    /// The Value that is being stored in state.
+    type Value;
+
+    /// At a given `StateId`, attempt to retrieve the given slice of keys.
+    ///
+    /// The results of the get will be returned in a `HashMap`.  Only keys that
+    /// were found will be in this map. Keys missing from the map can be
+    /// assumed to be missing from the underlying storage system as well.
+    ///
+    /// # Errors
+    ///
+    /// `StateReadError` is returned if any issues occur while trying to fetch
+    /// the values.
+    fn get(
+        &self,
+        state_id: &Self::StateId,
+        keys: &[Self::Key],
+    ) -> Result<HashMap<Self::Key, Self::Value>, StateReadError>;
+}
