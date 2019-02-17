077f89c067dd049c98e9654a24de7c3219790436
Shawn T. Amundson <amundson@bitwise.io>
2019-02-17 00:05:15 -0600

    Add a workload module and initial traits
    The TransactionWorkload and BatchWorkload traits are used to implement
    workload generators, which is useful for testing. In the future, this
    could provide a better basis for the CLI workload tool as well.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

---
+++ libtransact/src/workload/mod.rs
@@ -0,0 +1,28 @@
+pub mod error;
+
+use crate::batch::BatchPair;
+use crate::transaction::TransactionPair;
+use crate::workload::error::WorkloadError;
+
+pub trait TransactionWorkload {
+    fn next_transaction(&mut self) -> Result<TransactionPair, WorkloadError>;
+}
+
+pub trait BatchWorkload {
+    fn next_batch(&mut self) -> Result<BatchPair, WorkloadError>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    pub fn test_transaction_workload(workload: &mut TransactionWorkload) {
+        workload.next_transaction().unwrap();
+        workload.next_transaction().unwrap();
+    }
+
+    pub fn test_batch_workload(workload: &mut BatchWorkload) {
+        workload.next_batch().unwrap();
+        workload.next_batch().unwrap();
+    }
+}
