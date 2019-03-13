581aa78fd88ee44807b175c3e298f1652221048d
Eloá Franca Verona <eloafran@bitwise.io>
2019-03-13 10:58:50 -0500

    Add DatabaseReader and DatabaseCursor implementation to btree
    Signed-off-by: Eloá Franca Verona <eloafran@bitwise.io>

--- libtransact/src/database/btree.rs
+++ libtransact/src/database/btree.rs
@@ -15,9 +15,12 @@
  * -----------------------------------------------------------------------------
  */
 
+use crate::database::error::DatabaseError;
+use crate::database::{DatabaseCursor, DatabaseReader, DatabaseReaderCursor};
 use std::collections::BTreeMap;
 use std::collections::HashMap;
-use std::sync::{Arc, RwLock};
+use std::ops::Bound::{Excluded, Included};
+use std::sync::{Arc, RwLock, RwLockReadGuard};
 
 #[derive(Clone)]
 pub struct BTreeDatabase {
@@ -47,6 +50,129 @@ impl BTreeDbInternal {
         BTreeDbInternal {
             main: BTreeMap::new(),
             indexes: index_dbs,
+        }
+    }
+}
+
+pub struct BTreeReader<'a> {
+    db: RwLockReadGuard<'a, BTreeDbInternal>,
+}
+
+impl<'a> DatabaseReader for BTreeReader<'a> {
+    fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
+        match self.db.main.get(key) {
+            Some(value) => Some(value.to_vec()),
+            None => None,
+        }
+    }
+
+    fn index_get(&self, index: &str, key: &[u8]) -> Result<Option<Vec<u8>>, DatabaseError> {
+        match self
+            .db
+            .indexes
+            .get(index)
+            .ok_or_else(|| DatabaseError::ReaderError(format!("Not an index: {}", index)))?
+            .get(key)
+        {
+            Some(value) => Ok(Some(value.to_vec())),
+            None => Ok(None),
+        }
+    }
+
+    /// Returns a cursor against the main database. The cursor iterates over
+    /// the entries in the natural key order.
+    fn cursor(&self) -> Result<DatabaseCursor, DatabaseError> {
+        Ok(Box::new(BTreeDatabaseCursor::new(self.db.main.clone())))
+    }
+
+    /// Returns a cursor against the given index. The cursor iterates over
+    /// the entries in the index's natural key order.
+    fn index_cursor(&self, index: &str) -> Result<DatabaseCursor, DatabaseError> {
+        let index = self
+            .db
+            .indexes
+            .get(index)
+            .ok_or_else(|| DatabaseError::ReaderError(format!("Not an index: {}", index)))?;
+
+        Ok(Box::new(BTreeDatabaseCursor::new(index.clone())))
+    }
+
+    /// Returns the number of entries in the main database.
+    fn count(&self) -> Result<usize, DatabaseError> {
+        Ok(self.db.main.len())
+    }
+
+    /// Returns the number of entries in the given index.
+    fn index_count(&self, index: &str) -> Result<usize, DatabaseError> {
+        Ok(self
+            .db
+            .indexes
+            .get(index)
+            .ok_or_else(|| DatabaseError::ReaderError(format!("Not an index: {}", index)))?
+            .len())
+    }
+}
+
+pub struct BTreeDatabaseCursor {
+    db: BTreeMap<Vec<u8>, Vec<u8>>,
+    current_key: Option<Vec<u8>>,
+}
+
+impl BTreeDatabaseCursor {
+    pub fn new(db: BTreeMap<Vec<u8>, Vec<u8>>) -> BTreeDatabaseCursor {
+        BTreeDatabaseCursor {
+            db,
+            current_key: None,
+        }
+    }
+}
+
+impl DatabaseReaderCursor for BTreeDatabaseCursor {
+    fn first(&mut self) -> Option<(Vec<u8>, Vec<u8>)> {
+        match self.db.iter().next() {
+            Some((key, value)) => Some((key.to_vec(), value.to_vec())),
+            None => None,
+        }
+    }
+
+    fn last(&mut self) -> Option<(Vec<u8>, Vec<u8>)> {
+        match self.db.iter().last() {
+            Some((key, value)) => Some((key.to_vec(), value.to_vec())),
+            None => None,
+        }
+    }
+}
+
+impl Iterator for BTreeDatabaseCursor {
+    type Item = (Vec<u8>, Vec<u8>);
+
+    fn next(&mut self) -> Option<(Vec<u8>, Vec<u8>)> {
+        if self.current_key.is_none() {
+            match self.db.iter().next() {
+                Some((key, value)) => {
+                    self.current_key = Some(key.to_vec());
+                    return Some((key.to_vec(), value.to_vec()));
+                }
+                None => return None,
+            }
+        }
+        let last_key = match DatabaseReaderCursor::last(self) {
+            Some((key, _)) => key.to_vec(),
+            None => return None,
+        };
+        match self
+            .db
+            .range((
+                Excluded(self.current_key.clone().unwrap()),
+                Included(last_key),
+            ))
+            .next()
+        {
+            Some((key, value)) => {
+                self.current_key = Some(key.to_vec());
+                Some((key.to_vec(), value.to_vec()))
+            }
+            None => None,
         }
     }
 }
