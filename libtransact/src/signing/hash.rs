011b3b2616adc504be9d5836c90cf856e52d61c6
Peter Schwarz <pschwarz@bitwise.io>
2019-06-10 16:38:02 -0500

    Add simple module documentation for signing mods
    Additionally,
    
    - add missing license headers
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libtransact/src/signing/hash.rs
+++ libtransact/src/signing/hash.rs
@@ -1,3 +1,26 @@
+/*
+ * Copyright 2018 Bitwise IO, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -----------------------------------------------------------------------------
+ */
+
+//! A SHA-512 Hash Signer
+//!
+//! The HashSigner provides a simple implementation of the Signer trait, by simply producing a
+//! SHA-512 hash of the message bytes.  This implementation allows for the use of transact without
+//! the need of a cryptographic library for public-private key signing.
+
 use sha2::{Digest, Sha512};
 
 use crate::signing::Error;
