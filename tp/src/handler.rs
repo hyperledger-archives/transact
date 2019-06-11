d37509a6466e21af9eb11978fc3940fb9beb593f
Andrea Gunderson <agunde@bitwise.io>
2019-06-11 10:57:54 -0500

    Update version of sabre components to 0.4
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

--- tp/src/handler.rs
+++ tp/src/handler.rs
@@ -67,7 +67,7 @@ impl SabreTransactionHandler {
     pub fn new() -> SabreTransactionHandler {
         SabreTransactionHandler {
             family_name: "sabre".into(),
-            family_versions: vec!["0.3".into()],
+            family_versions: vec!["0.4".into()],
             namespaces: vec![
                 NAMESPACE_REGISTRY_PREFIX.into(),
                 CONTRACT_REGISTRY_PREFIX.into(),
