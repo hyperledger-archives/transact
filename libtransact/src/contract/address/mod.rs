e9441d3c1afadaf130ff706b39d5a50e2c9eeeea
Shannyn Telander <telander@bitwise.io>
2019-12-10 08:12:24 -0600

    Add Addresser trait
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- libtransact/src/contract/address/mod.rs
+++ libtransact/src/contract/address/mod.rs
@@ -18,3 +18,23 @@
 mod error;
 
 pub use crate::contract::address::error::AddresserError;
+
+pub const ADDRESS_LENGTH: usize = 70;
+
+pub trait Addresser<K> {
+    /// Returns a radix address calculated from the given key
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - A natural key, as defined by K, used to calculate an address
+    ///
+    fn compute(&self, key: &K) -> Result<String, AddresserError>;
+
+    /// Returns a human readable string of the given key
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - A natural key, as defined by K
+    ///
+    fn normalize(&self, key: &K) -> String;
+}
