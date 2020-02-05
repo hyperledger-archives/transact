f27be62693cc2d7d10150a04ea018088a8f60c25
Logan Seeley <seeley@bitwise.io>
2020-02-05 12:18:53 -0600

    Initialize contract archive module
    Initializes the `contract::archive` module, which is behind the
    experimental `contract-archive` feature flag.
    
    Signed-off-by: Logan Seeley <seeley@bitwise.io>

--- libtransact/src/contract/archive/mod.rs
+++ libtransact/src/contract/archive/mod.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Cargill Incorporated
+ * Copyright 2020 Cargill Incorporated
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,3 @@
  * limitations under the License.
  * -----------------------------------------------------------------------------
  */
-
-#[cfg(feature = "contract-address")]
-pub mod address;
-#[cfg(feature = "contract-archive")]
-pub mod archive;
-#[cfg(feature = "contract-context")]
-pub mod context;
