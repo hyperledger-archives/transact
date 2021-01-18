6199162951626dc061788b6ece0d57ca14e61b90
Andrea Gunderson <agunde@bitwise.io>
2021-01-18 15:42:27 -0600

    Update handler.rs Copyright header for 2021
    This file was modified in 2021, so update the copyright
    headers accordingly.
    
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

--- libtransact/src/handler/mod.rs
+++ libtransact/src/handler/mod.rs
@@ -1,6 +1,6 @@
 /*
  * Copyright 2017 Bitwise IO, Inc.
- * Copyright 2019 Cargill Incorporated
+ * Copyright 2019-2021 Cargill Incorporated
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
