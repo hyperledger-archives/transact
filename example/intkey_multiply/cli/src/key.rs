arsulegai/sawtooth-sabre

Support keyfile read from the intkey_multiply CLI
1. Support absolute keyfile read with extension other than `.priv`
2. Support relative keyfile read with any file extension.

Signed-off-by: S m, Aruna <aruna.mohan@walmartlabs.com>

example/intkey_multiply/cli/src/key.rs
@@ -18,18 +18,30 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-use users::get_current_username;
-
 use sawtooth_sdk::signing::secp256k1::Secp256k1PrivateKey;
+use std::path::PathBuf;
+use users::get_current_username;
 
 use crate::error::CliError;
 
 /// Return a signing key loaded from the user's environment
 ///
-/// This method attempts to load the user's key from a file.  The filename
-/// is constructed by appending ".priv" to the key's name.  If the name argument
-/// is None, then the USER environment variable is used in its place.
+/// This method attempts to load the user's key from a file.
+/// The input parameter ```key_param (K)``` works as follows.
+/// The parameter `key_param` is optional
 ///
+/// If the parameter is Some:
+///   1) The short-name translates to the ```keyfile``` at
+///      ${HOME}/.sawtooth/(K).priv
+///   2) If a ```keyfile``` as in point (1) is not found then a file
+///      ${HOME}/.sawtooth/{K} is searched for.
+///   3) If a ```keyfile``` as in point (2) also fails then a path
+///      {K} is searched for.
+///
+/// If the parameter is None:
+/// The USER environment variable is used as a key file identifier.
+/// The filename is constructed by appending ".priv" to the
+/// constructed key's name from the USER environment variable.
 /// The directory containing the keys is determined using the HOME
 /// environment variable:
 ///
@@ -37,8 +49,7 @@ use crate::error::CliError;
 ///
 /// # Arguments
 ///
-/// * `name` - The name of the signing key, which is used to construct the
-///            key's filename
+/// * `key_param` - The signing key parameter to be loaded
 ///
 /// # Errors
 ///
@@ -46,8 +57,8 @@ use crate::error::CliError;
 ///
 /// If a HOME or USER environment variable is required but cannot be
 /// retrieved from the environment, a CliError::VarError is returned.
-pub fn load_signing_key(name: Option<&str>) -> Result<Secp256k1PrivateKey, CliError> {
-    let username: String = name
+pub fn load_signing_key(key_param: Option<&str>) -> Result<Secp256k1PrivateKey, CliError> {
+    let derived_keyfile: String = key_param
         .map(String::from)
         .ok_or_else(|| env::var("USER"))
         .or_else(|_| get_current_username().ok_or(0))
@@ -57,7 +68,11 @@ pub fn load_signing_key(name: Option<&str>) -> Result<Secp256k1PrivateKey, CliEr
             ))
         })?;
 
-    let private_key_filename = dirs::home_dir()
+    // For the case Some(scenario 3)
+    let mut private_key_filename: PathBuf = PathBuf::from(&derived_keyfile);
+
+    // For the case Some(scenario 2)
+    let keyfile_identifier = dirs::home_dir()
         .ok_or_else(|| {
             CliError::UserError(String::from(
                 "Could not load signing key: unable to determine home directory",
@@ -66,9 +81,29 @@ pub fn load_signing_key(name: Option<&str>) -> Result<Secp256k1PrivateKey, CliEr
         .and_then(|mut p| {
             p.push(".sawtooth");
             p.push("keys");
-            p.push(format!("{}.priv", &username));
+            p.push(format!("{}", &derived_keyfile));
             Ok(p)
         })?;
+    if keyfile_identifier.as_path().exists() {
+        private_key_filename = keyfile_identifier;
+    }
+
+    // For the case Some(scenario 1) and None
+    let key_identifier = dirs::home_dir()
+        .ok_or_else(|| {
+            CliError::UserError(String::from(
+                "Could not load signing key: unable to determine home directory",
+            ))
+        })
+        .and_then(|mut p| {
+            p.push(".sawtooth");
+            p.push("keys");
+            p.push(format!("{}.priv", &derived_keyfile));
+            Ok(p)
+        })?;
+    if key_identifier.as_path().exists() {
+        private_key_filename = key_identifier;
+    }
 
     if !private_key_filename.as_path().exists() {
         return Err(CliError::UserError(format!(

hyperledger-archives/transact

example/intkey_multiply/cli/src/key.rs
@@ -18,18 +18,30 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-use users::get_current_username;
-
 use sawtooth_sdk::signing::secp256k1::Secp256k1PrivateKey;
+use std::path::PathBuf;
+use users::get_current_username;
 
 use crate::error::CliError;
 
 /// Return a signing key loaded from the user's environment
 ///
-/// This method attempts to load the user's key from a file.  The filename
-/// is constructed by appending ".priv" to the key's name.  If the name argument
-/// is None, then the USER environment variable is used in its place.
+/// This method attempts to load the user's key from a file.
+/// The input parameter ```key_param (K)``` works as follows.
+/// The parameter `key_param` is optional
 ///
+/// If the parameter is Some:
+///   1) The short-name translates to the ```keyfile``` at
+///      ${HOME}/.sawtooth/(K).priv
+///   2) If a ```keyfile``` as in point (1) is not found then a file
+///      ${HOME}/.sawtooth/{K} is searched for.
+///   3) If a ```keyfile``` as in point (2) also fails then a path
+///      {K} is searched for.
+///
+/// If the parameter is None:
+/// The USER environment variable is used as a key file identifier.
+/// The filename is constructed by appending ".priv" to the
+/// constructed key's name from the USER environment variable.
 /// The directory containing the keys is determined using the HOME
 /// environment variable:
 ///
@@ -37,8 +49,7 @@ use crate::error::CliError;
 ///
 /// # Arguments
 ///
-/// * `name` - The name of the signing key, which is used to construct the
-///            key's filename
+/// * `key_param` - The signing key parameter to be loaded
 ///
 /// # Errors
 ///
@@ -46,8 +57,8 @@ use crate::error::CliError;
 ///
 /// If a HOME or USER environment variable is required but cannot be
 /// retrieved from the environment, a CliError::VarError is returned.
-pub fn load_signing_key(name: Option<&str>) -> Result<Secp256k1PrivateKey, CliError> {
-    let username: String = name
+pub fn load_signing_key(key_param: Option<&str>) -> Result<Secp256k1PrivateKey, CliError> {
+    let derived_keyfile: String = key_param
         .map(String::from)
         .ok_or_else(|| env::var("USER"))
         .or_else(|_| get_current_username().ok_or(0))
@@ -57,7 +68,11 @@ pub fn load_signing_key(name: Option<&str>) -> Result<Secp256k1PrivateKey, CliEr
             ))
         })?;
 
-    let private_key_filename = dirs::home_dir()
+    // For the case Some(scenario 3)
+    let mut private_key_filename: PathBuf = PathBuf::from(&derived_keyfile);
+
+    // For the case Some(scenario 2)
+    let keyfile_identifier = dirs::home_dir()
         .ok_or_else(|| {
             CliError::UserError(String::from(
                 "Could not load signing key: unable to determine home directory",
@@ -66,9 +81,29 @@ pub fn load_signing_key(name: Option<&str>) -> Result<Secp256k1PrivateKey, CliEr
         .map(|mut p| {
             p.push(".sawtooth");
             p.push("keys");
-            p.push(format!("{}.priv", &username));
+            p.push(&derived_keyfile);
             p
         })?;
+    if keyfile_identifier.as_path().exists() {
+        private_key_filename = keyfile_identifier;
+    }
+
+    // For the case Some(scenario 1) and None
+    let key_identifier = dirs::home_dir()
+        .ok_or_else(|| {
+            CliError::UserError(String::from(
+                "Could not load signing key: unable to determine home directory",
+            ))
+        })
+        .map(|mut p| {
+            p.push(".sawtooth");
+            p.push("keys");
+            p.push(format!("{}.priv", &derived_keyfile));
+            p
+        })?;
+    if key_identifier.as_path().exists() {
+        private_key_filename = key_identifier;
+    }
 
     if !private_key_filename.as_path().exists() {
         return Err(CliError::UserError(format!(