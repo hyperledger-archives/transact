5a6d167aa479a54252de690550dad15fe8d58353
Peter Schwarz <pschwarz@bitwise.io>
2021-07-19 09:03:56 -0500

    Add postgres tests for get leaves operation
    This change adds tests for running the MerkleRadixGetLeavesOperation
    against a postgres connection.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libtransact/src/state/merkle/sql/operations/get_leaves.rs
+++ libtransact/src/state/merkle/sql/operations/get_leaves.rs
@@ -107,11 +107,17 @@ mod test {
 
     use diesel::dsl::insert_into;
 
+    #[cfg(feature = "state-merkle-sql-postgres-tests")]
+    use crate::state::merkle::sql::backend::postgres::test::run_postgres_test;
     use crate::state::merkle::sql::migration;
+    #[cfg(feature = "state-merkle-sql-postgres-tests")]
+    use crate::state::merkle::sql::models::postgres;
     use crate::state::merkle::sql::models::MerkleRadixLeaf;
     use crate::state::merkle::sql::operations::update_index::{
         ChangedLeaf, MerkleRadixUpdateIndexOperation,
     };
+    #[cfg(feature = "state-merkle-sql-postgres-tests")]
+    use crate::state::merkle::sql::schema::postgres_merkle_radix_tree_node;
 
     /// This tests that a leaf changed across several state root hashes is returned when requested
     /// at the given state root hash. It verifies that:
@@ -193,6 +199,88 @@ mod test {
         Ok(())
     }
 
+    /// This tests that a leaf changed across several state root hashes is returned when requested
+    /// at the given state root hash. It verifies that:
+    /// 1. No leaves are returned for the initial state root hash
+    /// 2. The first leaf change is returned for first state root.
+    /// 3. The second leaf change is returned for the second state root.
+    #[cfg(feature = "state-merkle-sql-postgres-tests")]
+    #[test]
+    fn postgres_get_leaves_at_state_root() -> Result<(), Box<dyn std::error::Error>> {
+        run_postgres_test(|url| {
+            let conn = PgConnection::establish(&url)?;
+
+            insert_state_root_nodes(&conn)?;
+
+            let operations = MerkleRadixOperations::new(&conn);
+
+            // insert the initial root:
+            insert_into(merkle_radix_state_root::table)
+                .values((
+                    merkle_radix_state_root::tree_id.eq(1),
+                    merkle_radix_state_root::state_root.eq("initial-state-root"),
+                    merkle_radix_state_root::parent_state_root.eq(""),
+                ))
+                .execute(&conn)?;
+
+            // insert the initial leaf
+            insert_into(merkle_radix_leaf::table)
+                .values(MerkleRadixLeaf {
+                    id: 1,
+                    tree_id: 1,
+                    address: "aabbcc".into(),
+                    data: b"hello".to_vec(),
+                })
+                .execute(&conn)?;
+
+            // update the index
+            operations.update_index(
+                1,
+                "first-state-root",
+                "initial-state-root",
+                vec![ChangedLeaf::AddedOrUpdated {
+                    address: "aabbcc",
+                    leaf_id: 1,
+                }],
+            )?;
+
+            // insert the changed leaf
+            insert_into(merkle_radix_leaf::table)
+                .values(MerkleRadixLeaf {
+                    id: 2,
+                    tree_id: 1,
+                    address: "aabbcc".into(),
+                    data: b"goodbye".to_vec(),
+                })
+                .execute(&conn)?;
+
+            operations.update_index(
+                1,
+                "second-state-root",
+                "first-state-root",
+                vec![ChangedLeaf::AddedOrUpdated {
+                    address: "aabbcc",
+                    leaf_id: 2,
+                }],
+            )?;
+
+            let leaves = operations.get_leaves(1, "initial-state-root", vec!["aabbcc"])?;
+            assert!(leaves.is_empty());
+
+            let leaves = operations.get_leaves(1, "first-state-root", vec!["aabbcc"])?;
+            assert_eq!(leaves.len(), 1);
+            assert_eq!(leaves[0].0, "aabbcc");
+            assert_eq!(leaves[0].1, b"hello");
+
+            let leaves = operations.get_leaves(1, "second-state-root", vec!["aabbcc"])?;
+            assert_eq!(leaves.len(), 1);
+            assert_eq!(leaves[0].0, "aabbcc");
+            assert_eq!(leaves[0].1, b"goodbye");
+
+            Ok(())
+        })
+    }
+
     /// This tests that a leaf inserted then deleted across several state root hashes is returned
     /// only on the state root hash where it exists.  It verifies that:
     /// 1. No leaves are returned for the initial state root hash
@@ -257,6 +345,74 @@ mod test {
         assert!(leaves.is_empty());
 
         Ok(())
+    }
+
+    /// This tests that a leaf inserted then deleted across several state root hashes is returned
+    /// only on the state root hash where it exists.  It verifies that:
+    /// 1. No leaves are returned for the initial state root hash
+    /// 2. The leaf is returned for first state root.
+    /// 3. The leaf is no longer returned for the second state root.
+    #[cfg(feature = "state-merkle-sql-postgres-tests")]
+    #[test]
+    fn postgres_get_leaves_with_deletion() -> Result<(), Box<dyn std::error::Error>> {
+        run_postgres_test(|url| {
+            let conn = PgConnection::establish(&url)?;
+
+            insert_state_root_nodes(&conn)?;
+
+            let operations = MerkleRadixOperations::new(&conn);
+
+            // insert the initial root:
+            insert_into(merkle_radix_state_root::table)
+                .values((
+                    merkle_radix_state_root::tree_id.eq(1),
+                    merkle_radix_state_root::state_root.eq("initial-state-root"),
+                    merkle_radix_state_root::parent_state_root.eq(""),
+                ))
+                .execute(&conn)?;
+
+            // insert the initial leaf
+            insert_into(merkle_radix_leaf::table)
+                .values(MerkleRadixLeaf {
+                    id: 1,
+                    tree_id: 1,
+                    address: "aabbcc".into(),
+                    data: b"hello".to_vec(),
+                })
+                .execute(&conn)?;
+
+            // update the index
+            operations.update_index(
+                1,
+                "first-state-root",
+                "initial-state-root",
+                vec![ChangedLeaf::AddedOrUpdated {
+                    address: "aabbcc",
+                    leaf_id: 1,
+                }],
+            )?;
+
+            // insert the changed leaf
+            operations.update_index(
+                1,
+                "second-state-root",
+                "first-state-root",
+                vec![ChangedLeaf::Deleted("aabbcc")],
+            )?;
+
+            let leaves = operations.get_leaves(1, "initial-state-root", vec!["aabbcc"])?;
+            assert!(leaves.is_empty());
+
+            let leaves = operations.get_leaves(1, "first-state-root", vec!["aabbcc"])?;
+            assert_eq!(leaves.len(), 1);
+            assert_eq!(leaves[0].0, "aabbcc");
+            assert_eq!(leaves[0].1, b"hello");
+
+            let leaves = operations.get_leaves(1, "second-state-root", vec!["aabbcc"])?;
+            assert!(leaves.is_empty());
+
+            Ok(())
+        })
     }
 
     /// This tests that several leaves, added against successive state root hashes.
@@ -337,6 +493,124 @@ mod test {
         assert_eq!(leaves[0].1, b"goodbye");
         assert_eq!(leaves[1].0, "aabbcc");
         assert_eq!(leaves[1].1, b"hello");
+
+        Ok(())
+    }
+
+    /// This tests that several leaves, added against successive state root hashes.
+    /// 1. Add a leaf and nodes to the tree at state root 1
+    /// 2. Add a second leaf and nodes to the tree at state root 2
+    /// 3. Verify that only the first leaf is returned with state root 1
+    /// 4. Verify that both leaves are returned with state root 2
+    #[cfg(feature = "state-merkle-sql-postgres-tests")]
+    #[test]
+    fn postgres_get_leaves_at_state_root_larger_tree() -> Result<(), Box<dyn std::error::Error>> {
+        run_postgres_test(|url| {
+            let conn = PgConnection::establish(&url)?;
+
+            insert_state_root_nodes(&conn)?;
+
+            let operations = MerkleRadixOperations::new(&conn);
+
+            // insert the initial root:
+            insert_into(merkle_radix_state_root::table)
+                .values((
+                    merkle_radix_state_root::tree_id.eq(1),
+                    merkle_radix_state_root::state_root.eq("initial-state-root"),
+                    merkle_radix_state_root::parent_state_root.eq(""),
+                ))
+                .execute(&conn)?;
+
+            // insert the initial leaf
+            insert_into(merkle_radix_leaf::table)
+                .values(MerkleRadixLeaf {
+                    id: 1,
+                    tree_id: 1,
+                    address: "aabbcc".into(),
+                    data: b"hello".to_vec(),
+                })
+                .execute(&conn)?;
+
+            // update the index
+            operations.update_index(
+                1,
+                "first-state-root",
+                "initial-state-root",
+                vec![ChangedLeaf::AddedOrUpdated {
+                    address: "aabbcc",
+                    leaf_id: 1,
+                }],
+            )?;
+
+            // insert a new leaf
+            insert_into(merkle_radix_leaf::table)
+                .values(MerkleRadixLeaf {
+                    id: 2,
+                    tree_id: 1,
+                    address: "112233".into(),
+                    data: b"goodbye".to_vec(),
+                })
+                .execute(&conn)?;
+
+            operations.update_index(
+                1,
+                "second-state-root",
+                "first-state-root",
+                vec![ChangedLeaf::AddedOrUpdated {
+                    address: "112233",
+                    leaf_id: 2,
+                }],
+            )?;
+
+            let leaves = operations.get_leaves(1, "initial-state-root", vec!["aabbcc"])?;
+            assert!(leaves.is_empty());
+
+            let leaves = operations.get_leaves(1, "first-state-root", vec!["aabbcc"])?;
+            assert_eq!(leaves.len(), 1);
+            assert_eq!(leaves[0].0, "aabbcc");
+            assert_eq!(leaves[0].1, b"hello");
+
+            let leaves = operations.get_leaves(1, "second-state-root", vec!["112233", "aabbcc"])?;
+            assert_eq!(leaves.len(), 2);
+            assert_eq!(leaves[0].0, "112233");
+            assert_eq!(leaves[0].1, b"goodbye");
+            assert_eq!(leaves[1].0, "aabbcc");
+            assert_eq!(leaves[1].1, b"hello");
+
+            Ok(())
+        })
+    }
+
+    #[cfg(feature = "state-merkle-sql-postgres-tests")]
+    fn insert_state_root_nodes(conn: &PgConnection) -> Result<(), Box<dyn std::error::Error>> {
+        insert_into(postgres_merkle_radix_tree_node::table)
+            .values(vec![
+                postgres::MerkleRadixTreeNode {
+                    hash: "initial-state-root".into(),
+                    tree_id: 1,
+                    leaf_id: None,
+                    children: vec![],
+                },
+                postgres::MerkleRadixTreeNode {
+                    hash: "new-state-root".into(),
+                    tree_id: 1,
+                    leaf_id: None,
+                    children: vec![],
+                },
+                postgres::MerkleRadixTreeNode {
+                    hash: "first-state-root".into(),
+                    tree_id: 1,
+                    leaf_id: None,
+                    children: vec![],
+                },
+                postgres::MerkleRadixTreeNode {
+                    hash: "second-state-root".into(),
+                    tree_id: 1,
+                    leaf_id: None,
+                    children: vec![],
+                },
+            ])
+            .execute(conn)?;
 
         Ok(())
     }
