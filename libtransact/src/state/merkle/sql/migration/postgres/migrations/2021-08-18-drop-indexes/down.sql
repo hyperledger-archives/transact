18c0689bf4cc693018d218047cef9c91f181068e
Peter Schwarz <pschwarz@bitwise.io>
2021-08-18 13:46:28 -0500

    Drop index tables
    This change drops the index tables, as they are no longer needed.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

---
+++ libtransact/src/state/merkle/sql/migration/postgres/migrations/2021-08-18-drop-indexes/down.sql
@@ -0,0 +1,33 @@
+-- Copyright 2021 Cargill Incorporated
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- -----------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS merkle_radix_state_root (
+    id BIGSERIAL PRIMARY KEY,
+    tree_id BIGINT NOT NULL,
+    state_root VARCHAR(64) NOT NULL,
+    parent_state_root VARCHAR(64) NOT NULL,
+    FOREIGN KEY(state_root, tree_id) REFERENCES merkle_radix_tree_node(hash, tree_id)
+);
+
+CREATE TABLE IF NOT EXISTS merkle_radix_state_root_leaf_index (
+    id BIGSERIAL PRIMARY KEY,
+    leaf_id BIGINT NOT NULL,
+    tree_id BIGINT NOT NULL,
+    from_state_root_id BIGINT NOT NULL,
+    to_state_root_id BIGINT,
+    FOREIGN KEY(from_state_root_id) REFERENCES merkle_radix_state_root(id),
+    FOREIGN KEY(leaf_id) REFERENCES merkle_radix_leaf (id),
+    FOREIGN KEY(tree_id) REFERENCES merkle_radix_tree (id)
+);
