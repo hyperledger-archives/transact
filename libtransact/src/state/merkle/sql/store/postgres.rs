39830680520885741c4d313484b423305cd9b9ca
Peter Schwarz <pschwarz@bitwise.io>
2022-02-08 15:46:08 -0600

    Use cache during get_leaves operation
    This change updates the get_leaves store operation to use an optional
    DataCache instance.
    
    The queries for both postgres and sqlite have been modified to not
    return the data if the hash of the entry matches the one contained in
    the cache.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libtransact/src/state/merkle/sql/store/postgres.rs
+++ libtransact/src/state/merkle/sql/store/postgres.rs
@@ -106,6 +106,8 @@ where
                 tree_id,
                 state_root_hash,
                 read_keys.iter().map(String::as_str).collect(),
+                #[cfg(feature = "state-merkle-sql-caching")]
+                self.cache,
             )
         })
     }
