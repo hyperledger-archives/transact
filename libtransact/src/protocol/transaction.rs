7db6fd31f176dff17fe37bbcc1d9f9180cfc7d56
Peter Schwarz <pschwarz@bitwise.io>
2022-02-28 17:19:41 -0600

    Fix unwrap_or_else_default lint
    This change fixes a lint introduced with the Rust 1.59 release where the
    use of unwrap_or_else with a closure returning the default value should
    be replaced with a simple call of unwrap_or_default.
    
    See
    https://rust-lang.github.io/rust-clippy/master/index.html#unwrap_or_else_default
    for more details.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libtransact/src/protocol/transaction.rs
+++ libtransact/src/protocol/transaction.rs
@@ -490,7 +490,7 @@ impl TransactionBuilder {
         let batcher_public_key = self
             .batcher_public_key
             .unwrap_or_else(|| signer_public_key.clone());
-        let dependencies = self.dependencies.unwrap_or_else(Vec::new);
+        let dependencies = self.dependencies.unwrap_or_default();
         let family_name = self.family_name.ok_or_else(|| {
             TransactionBuildError::MissingField("'family_name' field is required".to_string())
         })?;
