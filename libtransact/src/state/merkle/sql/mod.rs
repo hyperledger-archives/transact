a94106b6b4df29994f439dd0638d52def0603964
Peter Schwarz <pschwarz@bitwise.io>
2022-05-25 18:41:24 -0500

    Add state root hash to deletions
    This change adds the previous state root hash to the list of deletions
    when producing updates to the merkle tree.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libtransact/src/state/merkle/sql/mod.rs
+++ libtransact/src/state/merkle/sql/mod.rs
@@ -340,6 +340,8 @@ where
             batch.push((key_hash_hex.clone(), node, path));
         }
 
+        deletions.insert(self.state_root_hash.to_string());
+
         Ok((
             key_hash_hex,
             TreeUpdate {
